993a06aab1caac3996f2b85c0e0a20c92d4c4d8f
shabashik-tech <Admin4455785>
2021-01-06 14:47:21 +0300

    реформат кода в password_generator

--- password_generator/main.py
+++ password_generator/main.py
@@ -1,12 +1,11 @@
 import random
 
-length_password = int(input('Enter length your password: '))
-quantity_password = int(input('Enter quantity your password: '))
-data_char = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()'
-out_file = 'passwords.txt'
-
 
 def create_passwords():
+    """
+    Функция генерирующая пароли
+    :return: list
+    """
     passwords = []
     for quantity in range(quantity_password):
         password = ''
@@ -17,9 +16,20 @@ def create_passwords():
 
 
 def save_file(passwords, file_name):
+    """
+    Функция сохраняющая полученный list паролей в файл
+    :param passwords: list
+    :param file_name: str
+    """
     with open(file_name, 'a', encoding='utf-8') as file:
         for item in passwords:
             file.write('%s\n' % item) 
+
+
+length_password = int(input('Enter length your password: '))
+quantity_password = int(input('Enter quantity your password: '))
+data_char = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()'
+out_file = 'passwords.txt'
 
 
 def main():
